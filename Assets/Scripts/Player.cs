b4b312b56f0e2f37dbc73e31ff24fcb21b4bacdd
Aaron Hornby <aaron.hornby@outlook.com>
2018-11-01 17:50:10 -0600

    + Player script where all player data is stored.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public static Player instance = null; // singleton design pattern
+
+    private int paperSupply; // current amount of paper
+
+    // use for self init
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        else if (instance != this)
+        {
+            Destroy(gameObject); // destroy any attempts to create a duplicate
+        }
+
+        DontDestroyOnLoad(gameObject); // player data will exist throughout game
+
+    }
+
+    // use for init from other scripts
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void addPaper(int extraPaper)
+    {
+        paperSupply += extraPaper;
+    }
+
+    public void removePaper(int lostPaper)
+    {
+        paperSupply -= lostPaper;
+    }
+
+}
