0bd13a371280bd771ebc68ac2f07788a755b8dc8
Caelum Sloan <caelum.sloan1@ucalgary.ca>
2018-11-22 22:10:48 -0700

    Level Changer added

---
+++ Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelChanger : MonoBehaviour
+{
+    public List<Level> levels;
+    private IEnumerator<Level> level_tracker;
+
+    // Use this for initialization
+    void Start()
+    {
+        level_tracker = levels.GetEnumerator();
+        level_tracker.MoveNext();
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        // Level Switch
+        if (Time.time >= level_tracker.Current.length)
+        {
+            switchLevel();
+        }
+    }
+
+    private void switchLevel()
+    {
+        level_tracker.Current.towers.SetActive(true);
+        level_tracker.MoveNext();
+    }
+
+    [System.Serializable]
+    public class Level
+    {
+        public float length;
+        public GameObject towers;
+    }
+}
