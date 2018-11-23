ca16a7918ad013d236b2213a4ab761425e2e4b75
Daniel Doran <ddoran3@gmail.com>
2018-11-22 17:26:37 -0700

    move towers to their own folder and drop paper

--- Assets/Scripts/Tower/SimpleTower.cs
+++ Assets/Scripts/Tower/SimpleTower.cs
@@ -37,9 +37,4 @@ public class SimpleTower : BaseTower
 	{
 		throw new System.NotImplementedException();
 	}
-
-	public override void OnDeath()
-	{
-		throw new System.NotImplementedException();
-	}
 }
