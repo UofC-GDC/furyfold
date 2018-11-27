bd35757516e00b7560032916b822cb54cfaef16a
Caelum Sloan <caelum.sloan1@ucalgary.ca>
2018-11-26 22:57:06 -0700

    Organized main scene and created more prefabs

---
+++ Assets/Scripts/SpinInCircle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinInCircle : MonoBehaviour
+{
+    [Range(0,60f)]
+    [SerializeField] private float spinSpeed = 1f;
+
+    [SerializeField] private bool reverse = false;
+
+	void Update ()
+    {
+        var spin = reverse ? spinSpeed * -1f : spinSpeed;
+        transform.Rotate(Vector3.up, spin * Time.deltaTime, Space.Self);
+	}
+}
